cff407791d33e9d52c389e711833b37077e972aa
Rishabh <lordeadroid@users.noreply.github.com>
2023-05-03 00:08:35 +0530

    wrote test for addition of the complex number

--- test/complex-test.js
+++ test/complex-test.js
@@ -1,17 +1,26 @@
-const {strictEqual} = require("assert");
+const {strictEqual, deepStrictEqual} = require("assert");
 const {describe, it} = require("node:test");
-const {realPart, imaginaryPart} = require("../src/complex.js");
+const {add, complex} = require("../src/complex.js");
 
 describe("complex number", function() {
+  const firstNumber = complex(1, 2);
+  const secondNumber = complex(2, 3);
+
   describe("real part", function() {
     it("should give real part", function() {
-      strictEqual(realPart(2, 3), 2);
+      strictEqual(firstNumber.realPart(), 1);
     })
   })
 
   describe("imaginary part", function() {
     it("should give imaginary part", function() {
-      strictEqual(imaginaryPart(2, 3), 3);
+      strictEqual(firstNumber.imaginaryPart(), 2);
+    })
+  })
+
+  describe("addition", function() {
+    it("should give additon", function() {
+      deepStrictEqual(add(firstNumber, secondNumber), [3, 5]);
     })
   })
 })
