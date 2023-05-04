1074a89ea0b069d0bfe5a02e7f8407a67472eb37
Rishabh <lordeadroid@users.noreply.github.com>
2023-05-04 09:43:36 +0530

    added different test cases for each test case

--- test/complex-test.js
+++ test/complex-test.js
@@ -2,42 +2,43 @@ const {strictEqual, deepStrictEqual} = require("assert");
 const {describe, it} = require("node:test");
 const {complexNumber} = require("../src/complex.js");
 
-describe("complex number", function() {
-  const firstNumber = complexNumber(1, 2);
+describe("real part", function() {
+  const number = complexNumber(1, 2);
+  it("should give real part", function() {
+    strictEqual(number.realPart(), 1);
+  })
+})
+
+describe("imaginary part", function() {
+  const number = complexNumber(2, 4);
+  it("should give imaginary part", function() {
+    strictEqual(number.imaginaryPart(), 4);
+  })
+})
+
+describe("addition", function() {
+  const firstNumber = complexNumber(1, -1);
   const secondNumber = complexNumber(2, 3);
-
-  describe("real part", function() {
-    it("should give real part", function() {
-      strictEqual(firstNumber.realPart(), 1);
-    })
+  const sum = firstNumber.add(secondNumber);
+  it("should give sum of real and imaginary parts of numbers", function() {
+    strictEqual(sum.realPart(), 3);
+    strictEqual(sum.imaginaryPart(), 2);
   })
+})
 
-  describe("imaginary part", function() {
-    it("should give imaginary part", function() {
-      strictEqual(firstNumber.imaginaryPart(), 2);
-    })
+describe("multiplication", function() {
+  const firstNumber = complexNumber(4, 2);
+  const secondNumber = complexNumber(2, -3);
+  const product = firstNumber.multiply(secondNumber);
+  it("should give product of real and imaginary parts of numbers", function() {
+    strictEqual(product.realPart(), 14);
+    strictEqual(product.imaginaryPart(), -8); 
   })
+})
 
-  describe("addition", function() {
-    const sum = firstNumber.add(secondNumber);
-    it("should give sum of real and imaginary parts of numbers", function() {
-      strictEqual(sum.realPart(), 3);
-      strictEqual(sum.imaginaryPart(), 5);
-    })
-  })
-
-  describe("multiplication", function() {
-    const product = firstNumber.multiply(secondNumber);
-    it("should give product of real and imaginary parts of numbers", function() {
-      strictEqual(product.realPart(), -4);
-      strictEqual(product.imaginaryPart(), 7); 
-    })
-  })
-
-  describe("display", function() {
-    const number = complexNumber(1, 2);
-    it("should display complex number in the standard form", function() {
-      strictEqual(number.format(), "1 + 2i");
-    })
+describe("display", function() {
+  const number = complexNumber(1, 2);
+  it("should display complex number in the standard form", function() {
+    strictEqual(number.format(), "1 + 2i");
   })
 })
