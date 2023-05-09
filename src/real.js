d903291048718ac11f52fd61ecc64bdd5575f8e7
Rishabh <lordeadroid@users.noreply.github.com>
2023-05-10 00:41:44 +0530

    change implementation of all the test cases in real-test.js file

--- src/real.js
+++ src/real.js
@@ -3,29 +3,25 @@ class Real {
 
   constructor(number) {
     this.#real = number;
-  }
-
-  getReal() {
-    return this.#real;
-  }
+  };
 
   add(otherNumber) {
     const number = this.#real + otherNumber.#real;
     return new Real(number);
-  }
+  };
 
   multiply(otherNumber) {
     const number = this.#real * otherNumber.#real;
     return new Real(number);
-  }
+  };
 
   isEqual(otherNumber) {
     return this.#real === otherNumber.#real;
-  }
+  };
 
   toString() {
     return `${this.#real}`;
-  }
-}
+  };
+};
 
 exports.Real = Real;
--- test/real-test.js
+++ test/real-test.js
@@ -1,45 +1,65 @@
-const {deepStrictEqual} = require("assert");
-const {describe, it} = require("node:test");
+const { describe, it } = require("node:test");
+const { deepStrictEqual } = require("assert");
 
-const {Real} = require("../src/real.js");
+const { Real } = require("../src/real.js");
 
-describe("Real Number", function() {
-  it("get real", function() {
-    const number = new Real(2);
-    const actual = number.getReal();
-    const expected = 2;
-    deepStrictEqual(actual, expected);
-  })
+describe("Real Numbers", function () {
+  describe("addition:", function () {
+    it("should return addition of two numbers", function () {
+      const firstNumber = new Real(5);
+      const secondNumber = new Real(3);
+      const sum = firstNumber.add(secondNumber);
 
-  it("should return addition of two numbers", function() {
-    const number = new Real(5);
-    const otherNumber = new Real(3);
-    const actual = number.add(otherNumber);
-    const expected = new Real(8);
-    deepStrictEqual(actual, expected);
-  })
+      const actual = sum.isEqual(new Real(8));
+      const expected = true;
 
-  it("should return product of two numbers", function() {
-    const number = new Real(3);
-    const otherNumber = new Real(2);
-    const actual = number.multiply(otherNumber);
-    const expected = new Real(6);
-    deepStrictEqual(actual, expected);
-  })
+      deepStrictEqual(actual, expected);
+    });
+  });
 
-  it("should display number in standard form", function() {
-    const number = new Real(10);
-    const actual = number.toString();
-    const expected = '10';
-    deepStrictEqual(actual, expected);
-  })
+  describe("multiplication:", function () {
+    it("should return product of two numbers", function () {
+      const firstNumber = new Real(5);
+      const secondNumber = new Real(2);
+      const product = firstNumber.multiply(secondNumber);
 
-  it("should equate two numbers", function () {
-    const number = new Real(2);
-    const other = new Real(2);
+      const actual = product.isEqual(new Real(10));
+      const expected = true;
 
-    const actual = number.isEqual(other);
-    const expected = true;
-    deepStrictEqual(actual, expected);
-  })
-})
+      deepStrictEqual(actual, expected);
+    });
+  });
+
+  describe("toString:", function () {
+    it("should display number in string", function () {
+      const number = new Real(10);
+
+      const actual = number.toString();
+      const expected = '10';
+
+      deepStrictEqual(actual, expected);
+    });
+  });
+
+  describe("isEqual:", function () {
+    it("should be equal when same number is given", function () {
+      const firstNumber = new Real(3);
+      const secondNumber = new Real(3);
+
+      const actual = firstNumber.isEqual(secondNumber);
+      const expected = true;
+
+      deepStrictEqual(actual, expected);
+    });
+
+    it("should not be equal for different values", function () {
+      const firstNumber = new Real(1);
+      const secondNumber = new Real(2);
+
+      const actual = firstNumber.isEqual(secondNumber);
+      const expected = false;
+
+      deepStrictEqual(actual, expected);
+    });
+  });
+});
